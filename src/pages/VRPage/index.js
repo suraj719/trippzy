bccbd499c19f6e57d45945850657bcfb4b4d6f38
suraj pro <71000909+suraj719@users.noreply.github.com>
2024-04-19 23:33:16 +0530

    feat: temp imgs

--- src/pages/VRPage/index.js
+++ src/pages/VRPage/index.js
@@ -10,7 +10,97 @@ export default function VRPage() {
     "OodzQCbCdUkMeDr9HniDaG5geOQZT7JXrQmK30zP3BtfLoxvPl3CI0eQ"
   );
 
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({
+    photos: [
+      {
+        id: 1603650,
+        width: 5184,
+        height: 3456,
+        url: "https://www.pexels.com/photo/taj-mahal-and-the-four-minarets-1603650/",
+        photographer: "Sudipta  Mondal",
+        photographer_url: "https://www.pexels.com/@sudipta",
+        photographer_id: 581412,
+        avg_color: "#919A94",
+        src: {
+          original:
+            "https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg",
+          large2x:
+            "https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          large:
+            "https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+          medium:
+            "https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?auto=compress&cs=tinysrgb&h=350",
+          small:
+            "https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?auto=compress&cs=tinysrgb&h=130",
+          portrait:
+            "https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
+          landscape:
+            "https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
+          tiny: "https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280",
+        },
+        liked: false,
+        alt: "Taj Mahal and the Four Minarets",
+      },
+      {
+        id: 1530259,
+        width: 4160,
+        height: 6240,
+        url: "https://www.pexels.com/photo/low-angle-photo-of-eiffel-tower-1530259/",
+        photographer: "Louis",
+        photographer_url: "https://www.pexels.com/@suissounet",
+        photographer_id: 465924,
+        avg_color: "#6D9EA7",
+        src: {
+          original:
+            "https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg",
+          large2x:
+            "https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          large:
+            "https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+          medium:
+            "https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?auto=compress&cs=tinysrgb&h=350",
+          small:
+            "https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?auto=compress&cs=tinysrgb&h=130",
+          portrait:
+            "https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
+          landscape:
+            "https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
+          tiny: "https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280",
+        },
+        liked: false,
+        alt: "Low Angle Photo of Eiffel Tower",
+      },
+      {
+        id: 12968097,
+        width: 4000,
+        height: 6000,
+        url: "https://www.pexels.com/photo/view-of-the-charminar-at-sunset-12968097/",
+        photographer: "Satya Nandigam",
+        photographer_url: "https://www.pexels.com/@satya-nandigam-162270670",
+        photographer_id: 162270670,
+        avg_color: "#7C6965",
+        src: {
+          original:
+            "https://images.pexels.com/photos/12968097/pexels-photo-12968097.jpeg",
+          large2x:
+            "https://images.pexels.com/photos/12968097/pexels-photo-12968097.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          large:
+            "https://images.pexels.com/photos/12968097/pexels-photo-12968097.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+          medium:
+            "https://images.pexels.com/photos/12968097/pexels-photo-12968097.jpeg?auto=compress&cs=tinysrgb&h=350",
+          small:
+            "https://images.pexels.com/photos/12968097/pexels-photo-12968097.jpeg?auto=compress&cs=tinysrgb&h=130",
+          portrait:
+            "https://images.pexels.com/photos/12968097/pexels-photo-12968097.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
+          landscape:
+            "https://images.pexels.com/photos/12968097/pexels-photo-12968097.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
+          tiny: "https://images.pexels.com/photos/12968097/pexels-photo-12968097.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280",
+        },
+        liked: false,
+        alt: "View of the Charminar at Sunset",
+      },
+    ],
+  });
   const [query, setQuery] = useState("");
   const handleSearch = async () => {
     if (query) {
@@ -32,35 +122,36 @@ export default function VRPage() {
     setSelectedData(image);
     setSelectedImage(imageId);
   };
-
   return (
     <>
       <div className="border rounded-lg p-4 m-2 h-[85vh]">
-        <div className="flex items-center justify-center gap-8 mb-4">
+        <div className="flex items-center justify-center gap-8 mb-8">
           <input
             type="text"
             onChange={(e) => {
               setQuery(e.target.value);
             }}
             placeholder="Search for a location..."
-            className="w-[60%] p-2 border rounded-md"
+            className="w-[60%] p-2 bg-gray-100 rounded-md text-black"
           />
           <button
             onClick={handleSearch}
-            className="bg-green-500 rounded-lg p-2 font-bold"
+            className="bg-green-500 hover:bg-green-600 rounded-lg p-2 font-bold"
           >
             get pics ✨
           </button>
         </div>
-        {data?.photos?.length > 0 ? (
-          <>
-            <div
-              className="flex flex-wrap items-center justify-evenly gap-4"
-              style={{
-                maxHeight: "70vh",
-                overflow: "auto",
-              }}
-            >
+        {/* {data?.photos?.length > 0 ? (
+          <> */}
+        <div
+          className="flex flex-wrap items-center justify-evenly gap-4"
+          style={{
+            maxHeight: "70vh",
+            overflow: "auto",
+          }}
+        >
+          {data?.photos?.length > 0 ? (
+            <>
               {data?.photos?.map((image, index) => {
                 return (
                   <div key={index}>
@@ -110,11 +201,17 @@ export default function VRPage() {
                   </div>
                 );
               })}
-            </div>
-          </>
-        ) : (
+            </>
+          ) : (
+            <>
+              <p>sss</p>
+            </>
+          )}
+        </div>
+        {/* </> */}
+        {/* ) : (
           <></>
-        )}
+        )} */}
       </div>
     </>
   );
